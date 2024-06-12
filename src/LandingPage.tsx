78d9eee0c1570ec15abc7b8bb23c3b7ef7a9cd67
Elena Smeu <elena.smeu@skyspecs.com>
2024-06-12 12:54:11 +0200

    tailwind integration + Landing page init

---
+++ src/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function LandingPage() {
+  return (
+    <div className="flex flex-col h-full justify-center items-center">
+     Welcome on the landing page
+      
+    </div>
+  );
+}
+
+export default LandingPage;
