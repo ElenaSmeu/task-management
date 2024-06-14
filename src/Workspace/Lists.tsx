023a13a98de17a216a22214eca67e4dd098f7253
Elena Smeu <elena.smeu@skyspecs.com>
2024-06-14 13:11:24 +0200

    reactive

--- src/Workspace/Lists.tsx
+++ src/Workspace/Lists.tsx
@@ -1,32 +1,30 @@
-import { useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import AuthContext from "../Context/AuthContext";
 import { Board, List } from "../CRUD/models";
 import Button from "../SharedComponents/Button";
-import { faEllipsis, faEllipsisH, faPlus } from "@fortawesome/free-solid-svg-icons";
-import { UUID } from "crypto";
+import { faEllipsisH, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { createList, getListsOfBoard } from "../CRUD/lists";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Lists(props: {boardId: string}) {
     const [lists, setLists] = useState<List[]>([]);
-    const {currentUser, loading} = useContext(AuthContext);
     useEffect(() => {
       const fetchLists = async () => {
         const listsData = await getListsOfBoard(props.boardId);
         console.log(listsData);
-        //setLists(listsData);
+        setLists(listsData);
       };
       fetchLists();
-    }, []);
+    }, [props.boardId]);
     const handleCreateList = async () => {
         const newListId = await createList('NewList', props.boardId);
         const newList = { id: newListId, name: 'New List', boardId: props.boardId };
         setLists([...lists, newList]);
     }
     return (
-        <div className="flex flex-row items-start p-4">
+        <div className="flex flex-row w-screen overflow-x-auto items-start p-4">
             {lists.map(list => 
-              <div className="bg-green-900 border rounded-lg border-gray-800 border-4 w-80 p-4 mr-4">
+              <div className="bg-green-900 border rounded-lg border-gray-800 border-4 min-w-80 p-4 mr-4">
                   <div className="flex flex-col min-h-56">
                     <div className="flex flex-row text-gray-200 justify-between">
                       <div className="text-lg">{list.name}</div>
